0728583fcd864f2c2961a4f48fea36cf4246c813
Łukasz Demianiuk <ldemianiuk@gmail.com>
2019-10-22 19:43:44 +0200

    cleanup

--- src/app/caesar.service.spec.ts
+++ src/app/caesar.service.spec.ts
@@ -2,32 +2,32 @@ import { TestBed } from '@angular/core/testing';
 
 import { CaesarService } from './caesar.service';
 
+let service: CaesarService;
+
 describe('CaesarService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.get(CaesarService);
+  });
 
   it('should be created', () => {
-    const service: CaesarService = TestBed.get(CaesarService);
     expect(service).toBeTruthy();
   });
 
   it('should encrypt empty string', () => {
-    const service: CaesarService = TestBed.get(CaesarService);
     expect(service.encrypt('', 0)).toBe('');
     expect(service.encrypt('', 13)).toBe('');
   });
 
   it('should encrypt rot0', () => {
-    const service: CaesarService = TestBed.get(CaesarService);
     expect(service.encrypt('Hello, world!', 0)).toBe('Hello, world!');
   });
 
   it('should encrypt rot13', () => {
-    const service: CaesarService = TestBed.get(CaesarService);
     expect(service.encrypt('Hello, world!')).toBe('Uryyb, jbeyq!');
   });
 
   it('should encrypt rot39===rot13', () => {
-    const service: CaesarService = TestBed.get(CaesarService);
     expect(service.encrypt('Hello, world!', 39)).toBe('Uryyb, jbeyq!');
   });
 
--- src/app/caesar/caesar.component.spec.ts
+++ src/app/caesar/caesar.component.spec.ts
@@ -3,6 +3,9 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CaesarComponent } from './caesar.component';
 import {FormsModule} from '@angular/forms';
 
+const HELLO = 'Hello, world!';
+const HELLO_3 = 'Khoor, zruog!';
+
 describe('CaesarComponent', () => {
   let component: CaesarComponent;
   let fixture: ComponentFixture<CaesarComponent>;
@@ -24,4 +27,35 @@ describe('CaesarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should encrypt', () => {
+    component.key = 3;
+    component.plaintext = HELLO;
+    component.onPlaintextChange();
+    expect(component.encrypted).toBe(HELLO_3);
+  });
+
+  it('should decrypt', () => {
+    component.key = 3;
+    component.encrypted = HELLO_3;
+    component.onEncryptedChange();
+    expect(component.plaintext).toBe(HELLO);
+  });
+
+  it('should keep direction after key change', () => {
+    component.key = 3;
+    component.plaintext = '';
+    component.encrypted = '';
+    component.onPlaintextChange();
+    component.plaintext = HELLO;
+    component.onKeyChange();
+    expect(component.encrypted).toBe(HELLO_3);
+    component.plaintext = '';
+    component.encrypted = '';
+    component.onEncryptedChange();
+    component.encrypted = HELLO_3;
+    component.onKeyChange();
+    expect(component.plaintext).toBe(HELLO);
+  });
+
 });
